8822300875b209b457284392717f83d58995f9af
Darshan Sen <raisinten@gmail.com>
2023-03-10 17:58:19 +0530

    Add scripts for building curl and deps from source
    I tried referring to the `ngtcp2 version` section in
    https://curl.se/docs/http3.html but that runs into errors. After talking
    to the main author of curl on the curl mailing list, he pointed me to
    their working job which is tested on CI in
    https://curl.se/mail/lib-2023-03/0025.html. The scripts added here are
    roughly the same as the ones used there.
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

---
+++ scripts/build-curl.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+cp -R "$VENDOR/curl" "$BUILD"
+cd "$BUILD/curl"
+autoreconf -fi
+./configure \
+  --prefix=$PREFIX \
+  --with-openssl=$PREFIX \
+  PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig" \
+  LDFLAGS="-Wl,-rpath,$PREFIX/lib" \
+  --with-ngtcp2="$PREFIX" \
+  --enable-warnings \
+  --enable-werror \
+  --enable-debug \
+  --with-test-nghttpx="$PREFIX/bin/nghttpx"
+make CFLAGS="-mmacosx-version-min=10.15 -Wno-deprecated-declarations" install
