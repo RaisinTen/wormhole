b73b3305816986ddd6add2b75bd495fa0fa5a1c6
Darshan Sen <raisinten@gmail.com>
2023-03-25 12:20:19 +0530

    Resolve symlinks while copying libraries to relocatable zip
    This is simpler.
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- scripts/make-relocatable.sh
+++ scripts/make-relocatable.sh
@@ -26,26 +26,19 @@ cp -R "$PREFIX/include" .
 mkdir lib
 cd lib
 
-# Copy libraries with corresponding symlinks.
-# The symlinks are required so that during compilation, `-lcurl` could be passed
+# Copy libraries by resolving the symlinks, so that `-lcurl` could be passed
 # to the linker instead of `-lcurl.4`.
-cp "$PREFIX/lib/libcrypto.81.3.dylib" .
-cp -P "$PREFIX/lib/libcrypto.dylib" .
-cp "$PREFIX/lib/libcurl.4.dylib" .
-cp -P "$PREFIX/lib/libcurl.dylib" .
-cp "$PREFIX/lib/libnghttp3.3.dylib" .
-cp -P "$PREFIX/lib/libnghttp3.dylib" .
-cp "$PREFIX/lib/libngtcp2.10.dylib" .
-cp -P "$PREFIX/lib/libngtcp2.dylib" .
-cp "$PREFIX/lib/libngtcp2_crypto_openssl.4.dylib" .
-cp -P "$PREFIX/lib/libngtcp2_crypto_openssl.dylib" .
-cp "$PREFIX/lib/libssl.81.3.dylib" .
-cp -P "$PREFIX/lib/libssl.dylib" .
+cp "$PREFIX/lib/libcrypto.dylib" .
+cp "$PREFIX/lib/libcurl.dylib" .
+cp "$PREFIX/lib/libnghttp3.dylib" .
+cp "$PREFIX/lib/libngtcp2.dylib" .
+cp "$PREFIX/lib/libngtcp2_crypto_openssl.dylib" .
+cp "$PREFIX/lib/libssl.dylib" .
 
 # Set the install name of the entrypoint dylib - libcurl.
 # So here, the requirement is that whatever dylib / executable that attempts to
 # load this should have an rpath pointing to the `relocatable` directory.
-install_name_tool -id "@rpath/lib/libcurl.4.dylib" "libcurl.4.dylib"
+install_name_tool -id "@rpath/lib/libcurl.dylib" "libcurl.dylib"
 
 # Set the install names of the dylibs. This step is totally optional because
 # these libraries are currently only loaded by libraries in the
@@ -54,26 +47,26 @@ install_name_tool -id "@rpath/lib/libcurl.4.dylib" "libcurl.4.dylib"
 # reasons so that `otool -l` on these libraries don't contain the original paths
 # of these libraries which is only sensible on the host system where these were
 # built.
-install_name_tool -id "@rpath/lib/libcrypto.81.3.dylib" "libcrypto.81.3.dylib"
-install_name_tool -id "@rpath/lib/libnghttp3.3.dylib" "libnghttp3.3.dylib"
-install_name_tool -id "@rpath/lib/libngtcp2.10.dylib" "libngtcp2.10.dylib"
-install_name_tool -id "@rpath/lib/libngtcp2_crypto_openssl.4.dylib" "libngtcp2_crypto_openssl.4.dylib"
-install_name_tool -id "@rpath/lib/libssl.81.3.dylib" "libssl.81.3.dylib"
+install_name_tool -id "@rpath/lib/libcrypto.dylib" "libcrypto.dylib"
+install_name_tool -id "@rpath/lib/libnghttp3.dylib" "libnghttp3.dylib"
+install_name_tool -id "@rpath/lib/libngtcp2.dylib" "libngtcp2.dylib"
+install_name_tool -id "@rpath/lib/libngtcp2_crypto_openssl.dylib" "libngtcp2_crypto_openssl.dylib"
+install_name_tool -id "@rpath/lib/libssl.dylib" "libssl.dylib"
 
 # Change the dependent shared library install names in libcurl
-install_name_tool -change "$PREFIX/lib/libnghttp3.3.dylib" "@loader_path/libnghttp3.3.dylib" "libcurl.4.dylib"
-install_name_tool -change "$PREFIX/lib/libngtcp2_crypto_openssl.4.dylib" "@loader_path/libngtcp2_crypto_openssl.4.dylib" "libcurl.4.dylib"
-install_name_tool -change "$PREFIX/lib/libngtcp2.10.dylib" "@loader_path/libngtcp2.10.dylib" "libcurl.4.dylib"
-install_name_tool -change "$PREFIX/lib/libssl.81.3.dylib" "@loader_path/libssl.81.3.dylib" "libcurl.4.dylib"
-install_name_tool -change "$PREFIX/lib/libcrypto.81.3.dylib" "@loader_path/libcrypto.81.3.dylib" "libcurl.4.dylib"
+install_name_tool -change "$PREFIX/lib/libnghttp3.dylib" "@loader_path/libnghttp3.dylib" "libcurl.dylib"
+install_name_tool -change "$PREFIX/lib/libngtcp2_crypto_openssl.dylib" "@loader_path/libngtcp2_crypto_openssl.dylib" "libcurl.dylib"
+install_name_tool -change "$PREFIX/lib/libngtcp2.dylib" "@loader_path/libngtcp2.dylib" "libcurl.dylib"
+install_name_tool -change "$PREFIX/lib/libssl.dylib" "@loader_path/libssl.dylib" "libcurl.dylib"
+install_name_tool -change "$PREFIX/lib/libcrypto.dylib" "@loader_path/libcrypto.dylib" "libcurl.dylib"
 
 # Change the dependent shared library install names in libngtcp2_crypto_openssl
-install_name_tool -change "$PREFIX/lib/libngtcp2.10.dylib" "@loader_path/libngtcp2.10.dylib" "libngtcp2_crypto_openssl.4.dylib"
-install_name_tool -change "$PREFIX/lib/libssl.81.3.dylib" "@loader_path/libssl.81.3.dylib" "libngtcp2_crypto_openssl.4.dylib"
-install_name_tool -change "$PREFIX/lib/libcrypto.81.3.dylib" "@loader_path/libcrypto.81.3.dylib" "libngtcp2_crypto_openssl.4.dylib"
+install_name_tool -change "$PREFIX/lib/libngtcp2.dylib" "@loader_path/libngtcp2.dylib" "libngtcp2_crypto_openssl.dylib"
+install_name_tool -change "$PREFIX/lib/libssl.dylib" "@loader_path/libssl.dylib" "libngtcp2_crypto_openssl.dylib"
+install_name_tool -change "$PREFIX/lib/libcrypto.dylib" "@loader_path/libcrypto.dylib" "libngtcp2_crypto_openssl.dylib"
 
 # Change the dependent shared library install names in libssl
-install_name_tool -change "$PREFIX/lib/libcrypto.81.3.dylib" "@loader_path/libcrypto.81.3.dylib" "libssl.81.3.dylib"
+install_name_tool -change "$PREFIX/lib/libcrypto.dylib" "@loader_path/libcrypto.dylib" "libssl.dylib"
 
 # TODO(RaisinTen): Also include the system dependencies in the `relocatable/lib`
 # directory as relocatable libraries.
