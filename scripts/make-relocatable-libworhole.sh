7baa886ef4634df2aae31a4b3061905cf9a99686
Tony Gorez <gorez.tony@gmail.com>
2023-04-11 13:41:59 +0200

    refactor: abstract npm run cmake:build into a script (#76)

---
+++ scripts/make-relocatable-libworhole.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -o xtrace
+set -o errexit
+set -o nounset
+
+
+# This script makes libwormhole and the libraries it depends on relocatable, so that
+# it is possible to move the directory containing these libraries to another
+# location and still be able to link to these and use these correctly.
+
+# It will copy the dependency tree of libwormhole into the `relocatable_libwormhole` directory.
+cmake --install cmake_build --prefix relocatable_libwormhole
+
+# The install name of the entrypoint library is also changed into a path relative to the `@rpath`
+# path. The assumed `@rpath` here is the `relocatable_libwormhole` directory.
+install_name_tool -id @rpath/lib/libwormhole.dylib relocatable_libwormhole/lib/libwormhole.dylib
+
+# Replace the invalidated signature.
+# NOTE: We tried removing the signatures before running the `install_name_tool`
+# command but `install_name_tool` was failing with the error discussed in
+# https://stackoverflow.com/q/48324224:
+# install_name_tool: fatal error: file not in an order that can be processed (link edit information does not fill the __LINKEDIT segment)
+# Instead of using MachOView to fiddle with `__LINKEDIT`'s `File Size`, this is
+# using another approach that has been discussed in
+# https://bytemeta.vip/repo/ezQuake/ezquake-source/issues/624
+# i.e., to remove the invalidated signature after manipulating the binary and
+# resign it.
+codesign --remove-signature relocatable_libwormhole/lib/libwormhole.dylib
+codesign --sign - relocatable_libwormhole/lib/libwormhole.dylib
