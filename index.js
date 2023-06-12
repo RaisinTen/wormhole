a06f457aec81819ea48443cce805ba79c7aa4bf6
Darshan Sen <raisinten@gmail.com>
2023-06-12 18:41:50 +0530

    Implement EventEmitter-like API for WebSocket
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-const { request, websocketRequest } = require('./build/Release/wormhole.node');
+const { EventEmitter } = require('node:events');
+const {
+  request,
+  websocketRequest: _websocketRequest,
+} = require('./build/Release/wormhole.node');
+
+function websocketRequest(url) {
+  const handle = new EventEmitter();
+
+  let webSocket;
+  const req = _websocketRequest(url, (_webSocket, message) => {
+    // TODO(RaisinTen): Add support for 'open' event once the upstream
+    // discussion in the curl-library mailing list:
+    // https://curl.se/mail/lib-2023-06/0006.html
+    // gets resolved.
+    webSocket = _webSocket;
+    handle.emit('message', message);
+  });
+
+  req.then(() => {
+    handle.emit('close');
+  });
+
+  req.catch((err) => {
+    handle.emit('error', err);
+  });
+
+  handle.disconnect = function disconnect() {
+    if (!webSocket) {
+      throw new Error('Connection not established yet.');
+    }
+    webSocket.disconnect();
+  };
+
+  handle.send = function send(message) {
+    if (!webSocket) {
+      throw new Error('Connection not established yet.');
+    }
+    webSocket.send(message);
+  };
+
+  return handle;
+}
+
 module.exports = { request, websocketRequest };
